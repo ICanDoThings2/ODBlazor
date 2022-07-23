baba0a872b1901aecc416d1aa1817a86afc74096
Hunter Blake <HBlake002016@gmail.com>
2022-07-22 21:04:30 -0400

    Initial Commit
    Stuff

---
+++ ODBlazor/Models/DoBModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ODBlazor.Models
+{
+    public class DoBModel
+    {
+
+        [Required(ErrorMessage = "Date of Birth Required")]
+        public DateOnly BirthDate { get; set; }
+
+
+    }
+}

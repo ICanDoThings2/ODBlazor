9d91a3f107831ebb004e667d6dafad7510338f47
Hunter Blake <HBlake002016@gmail.com>
2022-08-02 22:20:27 -0400

    Back End First Commit
    Back-End Project Added. Still needs more work, including Authentication & Authorization.

---
+++ ODNetBackEnd/Models/UserDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ODNetBackEnd.Models
+{
+    public class UserDBContext : DbContext
+    {
+        public UserDBContext(DbContextOptions<UserDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
